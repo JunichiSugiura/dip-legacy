5623ba57d900307bed29c16e03ea8c6c7c7311b7
JunichiSugiura <jun.sugiura.jp@gmail.com>
2022-07-14 14:38:56 +0200

    cleanup

--- packages/desktop/src/plugin.rs
+++ packages/desktop/src/plugin.rs
@@ -1,5 +1,8 @@
 use crate::components::root;
-use bevy::app::{App, Plugin};
+use bevy::{
+    app::{App, Plugin},
+    window::WindowDescriptor,
+};
 use bevy_dioxus::desktop::prelude::*;
 use dip_core::{
     command::{CoreCommand, UICommand},
@@ -13,10 +16,8 @@ impl Plugin for DipDesktopPlugin {
         app.add_plugin(DipCorePlugin)
             .insert_resource(WindowDescriptor {
                 title: "dip".to_string(),
+                ..Default::default()
             })
-            .add_plugin(
-                DioxusPlugin::<EmptyGlobalState, CoreCommand, UICommand>::new(root::Root, ()),
-            )
-            .insert_non_send_resource(config);
+            .add_plugin(DioxusPlugin::<EmptyGlobalState, CoreCommand, UICommand>::new(root::Root));
     }
 }
